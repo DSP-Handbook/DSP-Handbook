d42974a023d13b92777ff68e70e74edc52c2c118
HuangFuSL <huangfusl@outlook.com>
2022-08-25 20:57:57 +0800

    Initialized repo structure

---
+++ make.py
@@ -0,0 +1,40 @@
+""" make.py for DSP-Handbook"""
+
+if __name__ == "__main__":
+    import os
+    import shutil
+    import subprocess
+    import argparse
+    import glob
+    import itertools
+
+    parser = argparse.ArgumentParser(
+        description="Build Document", add_help=False
+    )
+    parser.add_argument("action", choices=["build", "clean", "help"])
+    action = parser.parse_args().action
+
+    if action == "build":
+        os.chdir('src')
+        commit_id = subprocess.run(
+            ["git", "rev-parse", "--short", "HEAD"], capture_output=True, check=True
+        ).stdout.decode("utf-8").strip()
+        for _ in range(2):
+            subprocess.run([
+                "xelatex",
+                fr'\providecommand{{\commitId}}{{{commit_id}}}\input{{main}}'
+            ], check=True)
+
+        if not os.path.exists("../build"):
+            os.mkdir("../build")
+        shutil.copy('main.pdf', '../build')
+    elif action == "clean":
+        exts = [
+            ".aux", ".log", ".out", ".pdf", ".bcf",
+            ".run.xml", ".toc", ".ptc", ".synctex.gz"
+        ]
+        for _ in itertools.chain(*map(glob.glob, map(lambda ext: f"**/*{ext}", exts))):
+            os.remove(_)
+        shutil.rmtree("build", ignore_errors=True)
+    else:
+        parser.print_help()
